05f097b1dd20b1f830aa34347b1b89a308fb09e7
88byte <flaviod022@gmail.com>
2024-10-04 12:47:08 -0700

    Prepare for deploy

--- src/components/HunterSetup.js
+++ src/components/HunterSetup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate,u seLocation } from 'react-router-dom';
+import { useParams, useNavigate,useLocation } from 'react-router-dom';
 
 
 import { db, auth } from '../firebase';
