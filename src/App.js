2309dda56013a8bf2b745736937906a05de70086
88byte <flaviod022@gmail.com>
2024-10-10 09:30:29 -0700

    Switch to hash

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ const App = () => {
               <div className="hero-section">
                 <div className="hero-content">
                   <img src={wildLogo} alt="Wild Command Logo" className="hero-logo" />
-                  <h1 className="hero-title">Conquer the Wild...</h1>
+                  <h1 className="hero-title">Conquer the Wild</h1>
                   <h2 className="hero-subtitle">Command the Hunt.</h2>
                   <div className="hero-buttons">
                     <Link to="/signup">
