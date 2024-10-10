94ec8c877b5833f6346b89a4c31d28b8aa449e68
88byte <flaviod022@gmail.com>
2024-10-10 09:54:30 -0700

    Switch to hash

--- src/authContext.js
+++ src/authContext.js
@@ -40,32 +40,30 @@ export const AuthProvider = ({ children }) => {
       }
     };
 
-    useEffect(() => {
-      const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
-        if (currentUser) {
-          const tokenResult = await getIdTokenResult(currentUser);
-          const claims = tokenResult.claims;
-          console.log("Claims from token:", claims); // Add this to inspect the claims
-          setUser({
-            ...currentUser,
-            ...claims, // Merge the token claims with the user
-          });
-          
-          // Add another log to inspect the final user object
-          console.log("Authenticated user in AuthContext:", {
-            ...currentUser,
-            ...claims,
-          });
-        } else {
-          setUser(null);
-          console.log("No authenticated user");
-        }
-        setLoading(false);
-      });
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      if (currentUser) {
+        const tokenResult = await getIdTokenResult(currentUser);
+        const claims = tokenResult.claims;
+        console.log("Claims from token:", claims); // Add this to inspect the claims
+        setUser({
+          ...currentUser,
+          ...claims, // Merge the token claims with the user
+        });
+        
+        // Add another log to inspect the final user object
+        console.log("Authenticated user in AuthContext:", {
+          ...currentUser,
+          ...claims,
+        });
+      } else {
+        setUser(null);
+        console.log("No authenticated user");
+      }
+      setLoading(false);
+    });
 
-      return () => unsubscribe();
-    }, []);
-
+    return () => unsubscribe();
+  }, []);
 
   return (
     <AuthContext.Provider value={{ user }}>
